393bb1c0ee09533ad05b74f939f4ce42d8d92261
Ethan Nel <cethannel@protonmail.com>
2024-01-26 12:25:13 -0800

    Started Sidebar

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  type Link = {
+    svg: string;
+    text: string;
+  };
+</script>
+
 <aside class="w-64 border-r dark:border-gray-700 overflow-auto">
 	<h2 class="px-4 py-2 text-lg font-semibold border-b dark:border-gray-700">Categories</h2>
 	<nav class="flex flex-col gap-1 px-4 py-2">
