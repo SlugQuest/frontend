ce93afa5c62a5ce88bb1421286f51920f30e99ec
ethan <enel@uscs.edu>
2024-01-28 18:27:36 -0800

    Finished initial implementation of side bar

---
+++ src/lib/SideBarItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let path: string;
+	export let name: string;
+</script>
+
+<a
+	class="flex items-center gap-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+	href={path}
+>
+	<slot />
+	<span>{name}</span>
+</a>
