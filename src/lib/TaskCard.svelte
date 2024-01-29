76821b2c731a6bbd453918e81f78c48e94ee2dfb
Thomas Dillow <tdillow@ucsc.edu>
2024-01-28 21:24:14 -0800

    Also did topbar whoops who needs organization

--- src/lib/TaskCard.svelte
+++ src/lib/TaskCard.svelte
@@ -1,9 +1,14 @@
+<script lang="ts">
+	export let title: string;
+    export let description: string;
+</script>
+
 <div class="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
 	<div class="flex flex-col space-y-1.5 p-6">
 		<h3 class="text-2xl font-semibold whitespace-nowrap leading-none tracking-tight">
-			Finish report
+			{title}
 		</h3>
-		<p class="text-sm text-muted-foreground">Work</p>
+		<p class="text-sm text-muted-foreground">{description}</p>
 	</div>
 	<div class="p-6">
 		<div></div>
