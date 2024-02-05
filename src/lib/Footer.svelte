c7a37ca57288588f39fab896e6016c01971d9f21
sulayman <sasif@ucsc.edu>
2024-02-04 19:17:51 -0800

    created health bar

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,28 +1,45 @@
+<script>
+	let height = 25
+	let total_health = 600
+	let health_remaining = 100
+</script>
+
+<style>
+
+
+.container {
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		display: flex;
+		justify-content: center; 
+		align-items: center;
+	}
+	
+  .health {
+    	background-color: red;
+		position: absolute;
+		border: 3px solid black;
+		border-radius: 25px;
+  }
+
+	.bar {
+		background-color: darkgray;
+		position: absolute;
+		border: 3px solid black;
+		border-radius: 25px;
+	}
+</style>
+
+
 <footer class="flex items-center justify-between p-4 bg-white shadow-md">
 	<p class="text-sm text-gray-600 dark:text-gray-400">© 2024 SlugQuest</p>
 	<div class="flex items-center gap-4">
-		<!-- <svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="h-6 w-6 text-yellow-500"
-		>
-			<circle cx="12" cy="12" r="4"></circle>
-			<path d="M12 2v2"></path>
-			<path d="M12 20v2"></path>
-			<path d="m4.93 4.93 1.41 1.41"></path>
-			<path d="m17.66 17.66 1.41 1.41"></path>
-			<path d="M2 12h2"></path>
-			<path d="M20 12h2"></path>
-			<path d="m6.34 17.66-1.41 1.41"></path>
-			<path d="m19.07 4.93-1.41 1.41"></path>
-		</svg>
-		<p class="text-lg font-semibold">Light Mode</p> -->
+	</div>
+
+	<div class="container">
+		<div class = "bar" style="width: {total_health}px; height: {height}px;"> </div>
+		<div class="health" style="width: {health_remaining}px; height: {height}px;"></div>
+		
 	</div>
 </footer>
