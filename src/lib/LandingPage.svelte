78e9feb3ae5515077bfd085c67a1585d87f448be
Thomas Dillow <tdillow@ucsc.edu>
2024-02-09 09:28:25 -0800

    improved landing page

--- src/lib/LandingPage.svelte
+++ src/lib/LandingPage.svelte
@@ -7,8 +7,9 @@
 </script>
 
 <div class="flex items-center justify-center h-screen">
-    <div class="w-64">
-        <h1 class="text-2xl font-bold mb-4">Welcome</h1>
-        <button on:click={redirectToLogin} class="w-full px-4 py-2 bg-blue-600 text-white rounded-md">Log in</button>
+    <div class="w-64 text-center">
+        <h1 class="text-4xl font-bold mb-2">SlugQuest</h1>
+        <h2 class="text-2xl font-bold mb-4">Welcome</h2>
+        <button on:click={redirectToLogin} class="w-1/2 px-4 py-2 bg-blue-600 text-white rounded-md mx-auto">Log in</button>
     </div>
 </div>
